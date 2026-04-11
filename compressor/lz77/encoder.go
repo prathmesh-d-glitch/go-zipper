b8ba185713fa4ad434e5f08b1e2a6ef9494ad5bd
Prathmesh Dubey <prathmeshdubey10121973@gmail.com>
2026-04-11 18:33:30 +0530

    refactor Encode function for LZ77 compression: improve token generation logic and streamline variable initialization

--- compressor/lz77/encoder.go
+++ compressor/lz77/encoder.go
@@ -8,9 +8,81 @@ const (
 	MinMatchLength = 3
 	hashShift      = 5
 
-	hashSize = 1 << 15
-	hashMask = hashSize - 1
+	hashSize    = 1 << 15
+	hashMask    = hashSize - 1
+	maxChainLen = 128
 )
+
+func Encode(data []byte) []Token {
+	n := len(data)
+	if n == 0 {
+		return nil
+	}
+
+	tokens := make([]Token, 0, n/2)
+
+	head := make([]int, hashSize)
+
+	for i := range head {
+		head[i] = -1
+	}
+
+	prev := make([]int, n)
+
+	for i := range prev {
+		prev[i] = -1
+	}
+
+	pos := 0
+	for pos < n {
+		bestLen := 0
+		bestDist := 0
+
+		if pos+MinMatchLength <= n {
+			h := hash3(data, pos)
+
+			chainLen := 0
+			candidate := head[h]
+			windowStart := pos - WindowSize
+			if windowStart < 0 {
+				windowStart = 0
+			}
+			for candidate >= windowStart && chainLen < maxChainLen {
+				ml := matchLength(data, candidate, pos, n)
+				if ml > bestLen {
+					bestLen = ml
+					bestDist = pos - candidate
+					if bestLen == MaxMatchLength {
+						break
+					}
+				}
+				chainLen++
+				candidate = prev[candidate]
+			}
+
+			prev[pos] = head[h]
+			head[h] = pos
+		}
+
+		if bestLen >= MinMatchLength {
+			tokens = append(tokens, NewBackReference(bestDist, bestLen))
+
+			for i := 0; i < bestLen; i++ {
+				next := pos + i
+				if next+MinMatchLength <= n {
+					h := hash3(data, next)
+					prev[next] = head[h]
+					head[h] = next
+				}
+			}
+			pos += bestLen
+		} else {
+			tokens = append(tokens, NewLiteral(data[pos]))
+			pos++
+		}
+	}
+	return tokens
+}
 
 func hash3(data []byte, pos int) int {
 	return (int(data[pos])<<(2*hashShift) ^
