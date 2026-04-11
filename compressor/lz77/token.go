3ea155eeecdfbbd7c5a09b2842744e3dafbdb549
Prathmesh Dubey <prathmeshdubey10121973@gmail.com>
2026-04-11 11:53:31 +0530

    add Token type and associated methods for LZ77 compression

---
+++ compressor/lz77/token.go
@@ -0,0 +1,29 @@
+package lz77
+
+import "fmt"
+
+type Token struct {
+	isLiteral bool
+	Literal   byte
+	Distance  int
+	Length    int
+}
+
+func NewLiteral(b byte) Token {
+	return Token{isLiteral: true, Literal: b}
+}
+
+func NewBackReference(distance, length int) Token {
+	return Token{isLiteral: false, Distance: distance, Length: length}
+}
+
+//for debugging
+func (t *Token) String() string {
+	if t.isLiteral {
+		if t.Literal >= 0x20 && t.Literal <= 0x7E {
+			return fmt.Sprintf("Literal('%c')", t.Literal)
+		}
+		return fmt.Sprintf("Literal(0x%02X)", t.Literal)
+	}
+	return fmt.Sprintf("BackRef(d=%d, l=%d)", t.Distance, t.Length)
+}
