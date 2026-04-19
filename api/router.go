03d4ffbc3f7feca7ac69bbedb524c1ea50bf9d8c
Prathmesh Dubey <prathmeshdubey10121973@gmail.com>
2026-04-19 19:37:36 +0530

    add router: implement NewRouter function with Chi middleware for request handling

---
+++ api/router.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func NewRouter() chi.Router {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	return r
+}
