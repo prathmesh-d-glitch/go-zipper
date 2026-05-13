2fa1b293f95c5cd3b6de41e678fe383bb065c84f
Prathmesh Dubey <prathmeshdubey10121973@gmail.com>
2026-05-13 22:08:43 +0530

    fix: correct NewPool function implementation and worker initialization

--- worker/pool.go
+++ worker/pool.go
@@ -1,10 +1,67 @@
 package worker
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 type Pool struct {
 	tasks   chan Task
 	results chan Result
 	wg      sync.WaitGroup
 	once    sync.Once
+}
+
+func NewPool(newWorkers, queueSize int) *Pool {
+	if newWorkers < 1 {
+		newWorkers = 1
+	}
+	if queueSize < 1 {
+		queueSize = 1
+	}
+
+	p := &Pool{
+		tasks:   make(chan Task, queueSize),
+		results: make(chan Result, queueSize),
+	}
+
+	for i := 0; i < newWorkers; i++ {
+		p.wg.Add(1)
+		go p.worker(i)
+	}
+
+	return p
+}
+
+func (p *Pool) worker(id int) {
+	defer p.wg.Done()
+
+	for task := range p.tasks {
+		result := p.process(id, task)
+		p.results <- result
+	}
+}
+
+func (p *Pool) process(workerID int, task Task) (result Result) {
+	result.TaskID = task.ID
+	result.WorkerID = workerID
+
+	defer func() {
+		if r := recover(); r != nil {
+			result.Err = fmt.Errorf("worker %d: panic: %v", workerID, r)
+		}
+	}()
+
+	start := time.Now()
+
+	switch task.Type {
+	case TaskCompress:
+
+	case TaskDecompress:
+
+	default:
+	}
+	result.DurationMs = time.Since(start).Milliseconds()
+	return
 }
