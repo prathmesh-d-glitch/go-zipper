ff4442b2b2f765cfe47d07afb569b98cf48027ee
Prathmesh Dubey <prathmeshdubey10121973@gmail.com>
2026-05-14 18:51:11 +0530

    feat: implement separate processCompress and processDecompress methods for task handling

--- worker/pool.go
+++ worker/pool.go
@@ -2,8 +2,13 @@ package worker
 
 import (
 	"fmt"
+	"os"
 	"sync"
 	"time"
+
+	"github.com/prathmesh-d-glitch/go-zipper/compressor/huffman"
+	"github.com/prathmesh-d-glitch/go-zipper/compressor/lz77"
+	"github.com/prathmesh-d-glitch/go-zipper/utils"
 )
 
 type Pool struct {
@@ -57,11 +62,70 @@ func (p *Pool) process(workerID int, task Task) (result Result) {
 
 	switch task.Type {
 	case TaskCompress:
-
+		result = p.processCompress(workerID, task)
 	case TaskDecompress:
-
+		result = p.processDecompress(workerID, task)
 	default:
+		result.Err = fmt.Errorf("worker %d: unknown task type %q", workerID, task.Type)
 	}
 	result.DurationMs = time.Since(start).Milliseconds()
 	return
+}
+
+func (p *Pool) processCompress(workerID int, task Task) Result {
+	res := Result{TaskID: task.ID, WorkerID: workerID}
+
+	data, err := os.ReadFile(task.InputPath)
+	if err != nil {
+		res.Err = fmt.Errorf("reading %q: %w", task.InputPath, err)
+	}
+
+	res.BytesIn = int64(len(data))
+
+	crc := utils.Checksum(data)
+
+	tokens := lz77.Encode(data)
+	serialised := lz77.SerializeTokens(tokens)
+	compressed, err := huffman.Encode(serialised)
+	if err != nil {
+		res.Err = fmt.Errorf("comressing %q: %w", task.InputPath, err)
+		return res
+	}
+
+	res.Output = compressed
+	res.BytesOut = int64(len(compressed))
+
+	if task.Metadata == nil {
+		task.Metadata = make(map[string]string)
+	}
+	task.Metadata["crc32"] = fmt.Sprintf("%d", crc)
+
+	return res
+}
+
+func (p *Pool) processDecompress(workerID int, task Task) Result {
+	res := Result{TaskID: task.ID, WorkerID: workerID}
+	res.BytesIn = int64(len(task.InputData))
+
+	serialised, err := huffman.Decode(task.InputData)
+	if err != nil {
+		res.Err = fmt.Errorf("huffman decode: %w", err)
+		return res
+	}
+
+	tokens, err := lz77.DeserializeTokens(serialised)
+	if err != nil {
+		res.Err = fmt.Errorf("token deserialise: %w", err)
+		return res
+	}
+
+	original, err := lz77.Decode(tokens)
+	if err != nil {
+		res.Err = fmt.Errorf("lz77 decode: %w", err)
+		return res
+	}
+
+	res.Output = original
+	res.BytesOut = int64(len(original))
+	return res
 }
